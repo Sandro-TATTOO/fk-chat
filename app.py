5956f1076f656e27c3f416c176465c1ea03db8af
Sandro-TATTOO <sandroprogramatattoo@gemail.com>
2025-02-12 10:43:47 -0300

    Atualização de todos os arquivos com novas configurações

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from socketio_app import socketio  # Importando o objeto socketio
 from socket_handler import handle_message
-
-
-from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -13,15 +11,14 @@ def home():
     return render_template('index.html')
 
 @app.route('/chat')
+@app.route('/chat/pro')
+
 def chat():
     return render_template('chat.html')
-
 
 @socketio.on('message')
 def handle_message_event(message):
     handle_message(message)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
-
     socketio.run(app, debug=True)
--- socket_handler.py
+++ socket_handler.py
@@ -1,4 +1,14 @@
 from flask_socketio import emit
+from socketio_app import socketio  # Importando o objeto socketio
 
 def handle_message(message):
     emit('message', message, broadcast=True)
+
+@socketio.on('set_nickname')
+def set_nickname(nickname):
+    emit('nickname', nickname, broadcast=True)
+
+
+@socketio.on('image')
+def handle_image(image_blob):
+    emit('image', image_blob, broadcast=True)
